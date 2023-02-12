04cd6bf22a242b2ce75968d2ca59234fb8a16f2b
Nils Dannemann <ndannemann@gmail.com>
2023-02-12 11:36:07 +0100

    Initial commit

---
+++ src/layouts/LayoutDefault.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import Logo from '../components/Logo.vue';
+
+withDefaults(
+  defineProps<{
+    showFooter?: boolean;
+    animateLogo?: boolean;
+  }>(),
+  {
+    showFooter: true,
+    animateLogo: false,
+  }
+);
+</script>
+
+<template>
+  <div class="h-full flex flex-col">
+    <Logo :animate-in="animateLogo" class="mt-4" />
+    <main class="h-full relative">
+      <slot />
+    </main>
+    <footer v-if="showFooter" class="text-center uppercase mb-3">
+      <p class="text-white text-xs">Built in the future by</p>
+      <a class="text-blue drop-shadow-blue" href="https://pixelhop.io" target="_blank">Pixelhop</a>
+    </footer>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/supabase.ts
@@ -0,0 +1,11 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://mqkhqwljvmlmqwdblaeq.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  realtime: {
+    params: {
+      eventsPerSecond: 20,
+    },
+  },
+});
